7fbab3bcf661c915f8248bb19c45410c0d771e37
ellavrubel <webella@protonmail.com>
2019-12-05 19:55:37 +0300

    eve changes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
-    $('.active').before('<span>-----</span>');
+    // $('.active').before('<span>-----</span>');
 
     $('.owl-carousel').owlCarousel({
-        loop:true,
+        loop:false,
         margin:0,
         nav:true,
         smartSpeed:2000,
-        navText:['<img src="../img/EllipsePrev.png" alt="Prev">','<img src="../img/RectangleNext.png" alt="Next">'],
+        navText:[
+            '<img src="img/EllipsePrev.png" alt="Prev">',
+            '<img src="img/RectangleNext.png" alt="Next">'
+        ],
         responsive:{
             0: {
                 items: 1
             }
         }
-    })
+    });
+
+     // $('').after('<span>&frasl;</span>');
+
+
 });
